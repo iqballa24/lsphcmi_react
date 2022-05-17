bab761b3d34518061b3d31b8da1b82a130b5f41b
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-05-17 17:19:42 +0700

    constant variable

---
+++ src/data/constant/listOfLaw.js
@@ -0,0 +1,9 @@
+const listOfLaw = [
+  "UU No.3 th.2003 tentang ketenagakerjaan.",
+  "PP RI no.23 th.2004 tentang BNSP.",
+  "PP RI no.31 th.2006 tentang sistem pelatihan kerja nasional.",
+  "Peraturan Presiden Republik Indonesia no.8 th.2012 tentang kerangka kualifikasi Nasional Indonesia.",
+  "Peraturan Menteri Tenaga Kerja dan Transmigrasi Republik Indonesia no.5 th.2012 tentang Sistem Standarisasi Kompetensi Kerja Nasional.",
+];
+
+export default listOfLaw;
---
+++ src/data/constant/peserta.js
@@ -0,0 +1,30 @@
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+
+const peserta = [
+  {
+    icon: faUser,
+    desc: "BOD -1 s/d BOD -3",
+  },
+  {
+    icon: faUser,
+    desc: "Staff HRD",
+  },
+  {
+    icon: faUser,
+    desc: "Manajer",
+  },
+  {
+    icon: faUser,
+    desc: "Kepala Divisi",
+  },
+  {
+    icon: faUser,
+    desc: "Setiap orang yang ingin memiliki sertifikasi kompetensi HC",
+  },
+  {
+    icon: faUser,
+    desc: "BUMN, BUMD dan perusahaan swasta",
+  },
+];
+
+export default peserta;
